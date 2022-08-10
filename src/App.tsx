487e29c3da74f08f0a680e7765dd848e29d8a47a
Chris Thompson <chris@thompson-web.org>
2022-08-10 11:19:19 -0600

    feat: add MessageManager to manage message handlers
    listeners can be registered for specific messages
    
    listeners that return a value will cause a response to be sent back to the WebView

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { ActivityIndicator, SafeAreaView } from 'react-native'
-import WebView from 'react-native-webview'
+import { WebView } from 'react-native-webview'
 import { injectedJavaScript, onMessage } from './lib/console'
 import { MessageManager } from './lib/MessageManager'
+import { clearMnemonic, getMnemonic, hasMnemonic, setMnemonic } from './lib/mnemonicStore'
 import { shouldLoadFilter } from './lib/navigationFilter'
+import { SHAPESHIFT_URI } from 'react-native-dotenv'
 
 const backgroundColor = '#181c27'
 
@@ -14,15 +16,23 @@ const backgroundStyle = {
 
 /* Register message handlers and injected JavaScript */
 const messageManager = new MessageManager()
-messageManager.register('console', onMessage)
-messageManager.injectJavaScript(injectedJavaScript)
+messageManager.registerInjectedJavaScript(injectedJavaScript)
+
+messageManager.on('console', onMessage)
+messageManager.on('deleteKey', clearMnemonic)
+messageManager.on('getKey', getMnemonic)
+messageManager.on('hasKey', hasMnemonic)
+messageManager.on('setKey', setMnemonic)
 
 const App = () => {
   const [loading, setLoading] = useState(true)
+  const webviewRef = useRef<WebView>(null)
+  messageManager.setWebViewRef(webviewRef)
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <WebView
+        ref={webviewRef}
         // Hide the webview until the page is loaded
         style={[backgroundStyle, { display: loading ? 'none' : 'flex' }]}
         startInLoadingState
@@ -34,11 +44,11 @@ const App = () => {
           <ActivityIndicator color='#FFFFFF' size='large' style={backgroundStyle} />
         )}
         injectedJavaScriptBeforeContentLoaded={messageManager.injectedJavaScript}
-        onMessage={messageManager.onMessage.bind(messageManager)}
+        onMessage={msg => messageManager.handleMessage(msg)}
         onLoad={() => setLoading(false)}
         onNavigationStateChange={e => console.log('Navigation Start', e.url)}
         onShouldStartLoadWithRequest={shouldLoadFilter}
-        source={{ uri: 'https://app.shapeshift.com/#/dashboard' }}
+        source={{ uri: `${SHAPESHIFT_URI}/#/dashboard` }}
         // @TODO: Show an error screen
         onError={e => console.error(e)}
       />
