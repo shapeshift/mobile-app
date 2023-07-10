01d16c88d545a1385aebac1d5ab1fa75efda0e2e
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2023-07-10 16:19:52 +1000

    chore: remove unused added polyfill dependency

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,9 +2,6 @@ import App from './App'
 import { SingletonHooksContainer } from 'react-singleton-hook'
 import React from 'react'
 
-// import does the polyfilling, needed for new URLSearchParams
-import 'react-native-url-polyfill/auto'
-
 const Root: React.FC = () => {
   return (
     <>
