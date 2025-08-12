afc897a23eeda6ae6973489761884c9e88907c9a
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-08-12 16:00:53 +0200

    fix: lint

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -1,8 +1,7 @@
-import { useState, useEffect, useRef } from 'react';
-import { Text, View, Button, Platform } from 'react-native';
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
+import { Platform } from 'react-native'
+import * as Device from 'expo-device'
+import * as Notifications from 'expo-notifications'
+import Constants from 'expo-constants'
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -11,10 +10,10 @@ Notifications.setNotificationHandler({
     shouldShowBanner: true,
     shouldShowList: true,
   }),
-});
+})
 
 export async function registerForPushNotificationsAsync() {
-  let token;
+  let token
 
   if (Platform.OS === 'android') {
     await Notifications.setNotificationChannelAsync('shapeshiftAndroidChannel', {
@@ -22,39 +21,39 @@ export async function registerForPushNotificationsAsync() {
       importance: Notifications.AndroidImportance.MAX,
       vibrationPattern: [0, 250, 250, 250],
       lightColor: '#FF231F7C',
-    });
+    })
   }
 
   if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
+    const { status: existingStatus } = await Notifications.getPermissionsAsync()
+    let finalStatus = existingStatus
     if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
+      const { status } = await Notifications.requestPermissionsAsync()
+      finalStatus = status
     }
     if (finalStatus !== 'granted') {
-      return;
+      return
     }
     // Learn more about projectId:
     // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
     // EAS projectId is used here.
     try {
       const projectId =
-        Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+        Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId
       if (!projectId) {
-        throw new Error('Project ID not found');
+        throw new Error('Project ID not found')
       }
       token = (
         await Notifications.getExpoPushTokenAsync({
           projectId,
         })
-      ).data;
+      ).data
     } catch (e) {
-      token = `${e}`;
+      token = `${e}`
     }
   } else {
-    console.log('Must use physical device for Push Notifications');
+    console.log('Must use physical device for Push Notifications')
   }
 
-  return token;
+  return token
 }
